419201575fbab87c6b76b3f6472af3b822284e5b
Ezequiel Augusto Stanganelli <ezequielstanganelli@gmail.com>
2023-09-22 00:31:28 -0300

    Update main.cpp
    Signed-off-by: Ezequiel Augusto Stanganelli <ezequielstanganelli@gmail.com>

--- Proyecto/main/main.cpp
+++ Proyecto/main/main.cpp
@@ -1,8 +1,4 @@
-#include <QCoreApplication>
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-
-    return a.exec();
+int main(int argc, char *argv[]) {
+    
+    return 0;
 }
