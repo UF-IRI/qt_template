4879421de2b176ef82cc8b2fda66bd8e4dc325fd
Ezequiel Augusto Stanganelli <ezequielstanganelli@gmail.com>
2023-09-22 00:30:34 -0300

    Update main.cpp
    Signed-off-by: Ezequiel Augusto Stanganelli <ezequielstanganelli@gmail.com>

--- Proyecto/testing/main.cpp
+++ Proyecto/testing/main.cpp
@@ -1,9 +1,6 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch2/catch.hpp>
-#include <QtGui/QGuiApplication>
 
-int main(int argc, char** argv)
-{
-    QGuiApplication app(argc, argv);
+int main(int argc, char** argv) {
     return Catch::Session().run(argc, argv);
 }
